b5aa8dba6f09422bc1c1e294fb6a70a4c76f94bb
suchjose84 <suchjose@live.com>
2023-06-13 00:04:03 +0800

    fix fix

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,40 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema');
 const { requiresAuth } = require('express-openid-connect');
 
-
-routes.use('/', (req, res, next) => {
-  if (req.oidc.isAuthenticated()) {
-    // User is authenticated
-    let docData = {
-      // documentationURL: 'https://cse341-mw5a.onrender.com/api-docs',
-      message: 'You are logged in! API doc is at https://cse341-mw5a.onrender.com/api-docs'
-    };
-    res.send(docData);
-  } else {
-    // User is not authenticated
-    let docData = {
-      // documentationURL: 'https://cse341-mw5a.onrender.com/api-docs',
-      message: 'Welcome guest! Please login.'
-    };
-    res.send(docData);
+routes.use('/', requiresAuth(), async (req, res, next) => {
+  try {
+    if (req.oidc.isAuthenticated()) {
+      // User is authenticated
+      let docData = {
+        // documentationURL: 'https://cse341-mw5a.onrender.com/api-docs',
+        message: 'You are logged in! API doc is at https://cse341-mw5a.onrender.com/api-docs'
+      };
+      res.send(docData);
+    } else {
+      // User is not authenticated
+      let docData = {
+        // documentationURL: 'https://cse341-mw5a.onrender.com/api-docs',
+        message: 'Welcome guest! Please login.'
+      };
+      res.send(docData);
+    }
+  } catch (error) {
+    next(error);
   }
 });
-routes.use('/graphql', (req, res, next) => {
-  // Check if user is authenticated
-  if (req.oidc.isAuthenticated()) {
-    // User is authenticated, proceed to GraphQL endpoint
-    next();
-  } else {
-    // User is not authenticated, return unauthorized response
-    res.status(401).json({ message: 'Unauthorized' });
+
+routes.use('/graphql', requiresAuth(), async (req, res, next) => {
+  try {
+    // Check if user is authenticated
+    if (req.oidc.isAuthenticated()) {
+      // User is authenticated, proceed to GraphQL endpoint
+      next();
+    } else {
+      // User is not authenticated, return unauthorized response
+      res.status(401).json({ message: 'Unauthorized' });
+    }
+  } catch (error) {
+    next(error);
   }
 }, graphqlHTTP({
   schema,
