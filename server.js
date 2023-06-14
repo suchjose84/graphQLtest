15a48aee41fb50eb06fbfe9bb0ab7e4b7bb2932b
suchjose84 <suchjose@live.com>
2023-06-14 23:07:19 +0800

    fix fix

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-const schema = require('./schema/index.js');
-const {auth} = require('express-openid-connect');
-const { requiresAuth } = require('express-openid-connect');
-const authConfig = require('./config/auth0.config.js');
-const port = process.env.PORT || 3000;
+// const express = require('express');
+// const {graphqlHTTP} = require('express-graphql');
+// const schema = require('./schema/index.js');
+// const {auth} = require('express-openid-connect');
+// const { requiresAuth } = require('express-openid-connect');
+// const authConfig = require('./config/auth0.config.js');
+// const port = process.env.PORT || 3000;
 
 
-const app = express();
+// const app = express();
 
-// connects to auth0
-app.use(auth(authConfig.config));
+// // connects to auth0
+// app.use(auth(authConfig.config));
 
 // app.use('/', (req, res, next) => {
 //   if (req.oidc.isAuthenticated()) {
@@ -30,13 +30,58 @@ app.use(auth(authConfig.config));
 //   }
 // });
 
-//graphiql
+// //graphiql
+// app.use('/graphql', requiresAuth(), graphqlHTTP({
+//   schema: schema,
+//   graphiql: true
+// }));
+
+// //connect to db
+// const db = require('./models');
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(`Connected to the database and server running on port ${port}.`);
+//     });
+//   })
+//   .catch(err => {
+//     console.log('Cannot connect to the database!', err);
+//     process.exit();
+//   });
+
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/index.js');
+const { auth, requiresAuth } = require('express-openid-connect');
+const authConfig = require('./config/auth0.config.js');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// Connects to auth0
+app.use(auth(authConfig.config));
+
+app.get('/', (req, res) => {
+  if (req.oidc.isAuthenticated()) {
+    // User is authenticated
+    res.send('You are logged in! API doc is at https://graphqltest.onrender.com');
+  } else {
+    // User is not authenticated
+    res.send('Welcome guest! Please login.');
+  }
+});
+
+// GraphiQL
 app.use('/graphql', requiresAuth(), graphqlHTTP({
   schema: schema,
   graphiql: true
 }));
 
-//connect to db
+// Connect to db
 const db = require('./models');
 db.mongoose
   .connect(db.url, {
@@ -52,3 +97,5 @@ db.mongoose
     console.log('Cannot connect to the database!', err);
     process.exit();
   });
+
+
