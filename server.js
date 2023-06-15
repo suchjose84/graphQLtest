2e52617fcb66b3e952be91e3baae5ddc00684389
suchjose84 <suchjose@live.com>
2023-06-15 16:48:31 +0800

    add ts

--- server.js
+++ server.js
@@ -1,58 +1,3 @@
-// const express = require('express');
-// const {graphqlHTTP} = require('express-graphql');
-// const schema = require('./schema/index.js');
-// const {auth} = require('express-openid-connect');
-// const { requiresAuth } = require('express-openid-connect');
-// const authConfig = require('./config/auth0.config.js');
-// const port = process.env.PORT || 3000;
-
-
-// const app = express();
-
-// // connects to auth0
-// app.use(auth(authConfig.config));
-
-// app.use('/', (req, res, next) => {
-//   if (req.oidc.isAuthenticated()) {
-//     // User is authenticated
-//     let docData = {
-//       // documentationURL: 'https://cse341-mw5a.onrender.com/api-docs',
-//       message: 'You are logged in! API doc is at https://graphqltest.onrender.com'
-//     };
-//     res.send(docData);
-//   } else {
-//     // User is not authenticated
-//     let docData = {
-//       // documentationURL: 'https://cse341-mw5a.onrender.com/api-docs',
-//       message: 'Welcome guest! Please login.'
-//     };
-//     res.send(docData);
-//   }
-// });
-
-// //graphiql
-// app.use('/graphql', requiresAuth(), graphqlHTTP({
-//   schema: schema,
-//   graphiql: true
-// }));
-
-// //connect to db
-// const db = require('./models');
-// db.mongoose
-//   .connect(db.url, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(() => {
-//     app.listen(port, () => {
-//       console.log(`Connected to the database and server running on port ${port}.`);
-//     });
-//   })
-//   .catch(err => {
-//     console.log('Cannot connect to the database!', err);
-//     process.exit();
-//   });
-
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/index.js');
@@ -76,13 +21,13 @@ app.get('/', (req, res) => {
 });
 
 // GraphiQL
-app.use('/graphql', requiresAuth(), graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
 }));
 
 // Connect to db
-const db = require('./models');
+const db = require('./models/index.js');
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -97,5 +42,7 @@ db.mongoose
     console.log('Cannot connect to the database!', err);
     process.exit();
   });
+
+
 
 
